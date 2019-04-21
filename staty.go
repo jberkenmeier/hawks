c82b5c3420bf4000d5625bc21ff4e6f931ce1df9
BBurr18 <brandonburr@u.boisestate.edu>
2019-04-21 15:15:47 -0600

    it at least counts the lines

---
+++ staty.go
@@ -0,0 +1,79 @@
+package main
+
+//import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+var p = fmt.Print
+
+/*
+ Main function for the program. It will run through a given text file and scan all of the words, lines, and characters and return statistics about them.
+*/
+func main() {
+	//filename := os.Args[1]
+	var c chan int = make(chan int)
+	//Create the concurrency. When go is called, the method is run and the next line starts executing. This will start
+	//the scanning of words, characters, and lines running at the same time
+
+	go scanWords(c)
+	//go scanCharacters(c)
+	go scanLines(c)
+	go printer(c)
+	for {
+
+	}
+}
+
+func printer(c chan int) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(1100)
+	}
+}
+
+/*
+ This function will do the scanning of words and print the statistics associated with it.
+*/
+func scanWords(c chan int) {
+	var linecount = 0
+	file, _ := os.Open("alice.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		linecount++
+	}
+	p("LineCount: ")
+	c <- linecount
+
+}
+
+/*
+ This function will do the scanning of characters and print the statistics associated with it.
+*/
+/*func scanCharacters(c chan string) {
+	for x := 0; x <= 100; x++ {
+		c <- "characters"
+	}
+}/*
+
+/*
+ This function will do the scanning of lines and print the statistics associated with it.
+*/
+func scanLines(c chan int) {
+	var linecount = 0
+	file, _ := os.Open("alice.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		linecount++
+	}
+	p("LineCount: ")
+	c <- linecount
+
+}
+
